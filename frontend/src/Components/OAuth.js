35be81840719ac275e08ae77e031b5a8ea318b4d
Etienne Jacquot <etienne.jacquot@asc.upenn.edu>
2022-03-28 23:18:07 -0400

    sample oauth page no redirect or anything..

---
+++ frontend/src/Components/OAuth.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+//const enteredName = prompt('Please enter your name')
+
+const OAuth = () => {
+
+    return (
+            <div>
+                <h3>REDIRECT FOR INSTAGRAM (META) API ALLOW
+                    <a href="https://instagram.com" rel="oauth"> instagram.com (TBD) </a> 
+                </h3>
+            </div>
+            )
+    };
+export default OAuth;
